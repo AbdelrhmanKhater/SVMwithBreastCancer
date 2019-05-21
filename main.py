0ace8941a64a9750f7947a70707f9d1bd4d6d45b
ak7 <abdelrhman.khater111@gmail.com>
2019-05-22 00:54:32 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 21 03:53:02 2019
+
+@author: Dell
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+bankdata = pd.read_csv(b"C:\Users\Dell\PycharmProjects\breast_cancer")
+
